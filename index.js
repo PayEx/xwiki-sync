0e61f44608b346bce3f322c0f1a3b34093f013de
Håkon Mosvoll <hakon.mosvoll@payex.com>
2019-03-15 14:14:44 +0100

    Successfully posted to XWiki

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-console.log("Hello world");
+var http = require("http"),
+    fs = require('fs'),
+    path = require('path');
+
+var auth = Buffer.from("Admin:admin").toString("base64"),
+    fileName = "foo",
+    fileExtension = "xml",
+    filePath = path.join(__dirname, fileName + "." + fileExtension);
+
+fs.readFile(filePath, { encoding: 'utf-8' }, function (error, data){
+    var postRequest = http.request({
+        hostname: "localhost",
+        port: 8080,
+        path: "/xwiki/rest/wikis/xwiki/spaces/Sandbox/pages/" + fileName,
+        method: "PUT",
+        headers: {
+            "Authorization": "Basic " +  auth,
+            "Content-Type": "application/xml",
+            "Allow": "application/json"
+        }
+    }, (res) => {
+        res.setEncoding('utf8');
+        res.on('data', function (chunk) {
+            console.log('Response: ' + chunk);
+        });
+    });
+    
+    postRequest.write(data);
+    postRequest.end();
+});
+
