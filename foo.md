b5e6848a92896505280ad835c69a7bf727a41f12
Håkon Mosvoll <hakon.mosvoll@payex.com>
2019-04-26 16:46:36 +0200

    wip.

--- foo.md
+++ foo.md
@@ -1,5 +1,5 @@
-# Foo Page
 This is a new page from the Node Service
 - list item 1
 - list item 2
 - list item 3
+- list item 4
--- index.js
+++ index.js
@@ -2,17 +2,70 @@ var http = require("http"),
     fs = require('fs'),
     path = require('path');
 
-var auth = Buffer.from("Admin:admin").toString("base64"),
+let configuration = {
+    user: null,
+    password: null,
+    url: null
+};
+
+process.argv.forEach(function(argument, index){
+    // Node path and script name is included in argv
+    if(index >= 2){ return; }
+
+    console.log(argument);
+
+    var argumentMatch = argument.match(/--([a-z]+)=(\S+)/);
+
+    console.log(argumentMatch);
+
+    if(argumentMatch === null){
+        new Error("Invalid argument " + argument);
+    }
+
+    var argumentName = argumentMatch[1];
+    var argumentValue = argumentMatch[2];
+    
+    switch (argumentName){
+        case "user":
+            configuration.user = argumentValue;
+            break;
+        case "password":
+            configuration.password = argumentValue;
+            break;
+        case "wiki":
+            configuration.url = new URL(argumentValue)
+            break;
+        default:
+            new Error("Unrecognized argument " + argumentName + " with value " + argumentValue);
+    }
+});
+
+if(configuration.user === null){
+    new Error("Missing confiugration for user");
+}
+
+if(configuration.password === null){
+    new Error("Missing confiugration for password");
+}
+
+if(configuration.url === null){
+    new Error("Missing confiugration for url");
+}
+
+console.log("Configuration");
+console.log(configuration);
+
+var auth = Buffer.from(configuration.user + ":" + configuration.password).toString("base64"),
     fileName = "foo",
     fileExtension = "md",
-    wikiTitle = "foo-test-two",
+    wikiTitle = "foo-test-18",
     filePath = path.join(__dirname, fileName + "." + fileExtension);
 
 fs.readFile(filePath, { encoding: 'utf-8' }, function (error, data){
     var postRequest = http.request({
-        hostname: "localhost",
-        port: 8080,
-        path: "/xwiki/rest/wikis/xwiki/spaces/Sandbox/pages/" + wikiTitle,
+        hostname: configuration.url.hostname,
+        port: configuration.url.port,
+        path: configuration.url.pathName + wikiTitle,
         method: "PUT",
         headers: {
             "Authorization": "Basic " +  auth,
@@ -28,6 +81,7 @@ fs.readFile(filePath, { encoding: 'utf-8' }, function (error, data){
     
     var requestData = JSON.stringify({
         title: wikiTitle,
+        creator: "TestTest",
         syntax: "markdown/1.2",
         content: data 
     });
